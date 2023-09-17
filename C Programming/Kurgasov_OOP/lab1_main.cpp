789888b8e04a22ddc99186c2e39180671ec931fc
Manannikov Anton <anton2manan2@gmail.com>
2023-09-17 12:12:05 +0300

    sync 17.09.23 from mac

--- C Programming/Kurgasov_OOP/lab1_main.cpp
+++ C Programming/Kurgasov_OOP/lab1_main.cpp
@@ -2,10 +2,11 @@
 #include<vector>
 #include<string>
 #include<regex>
-#include<ctime>
+#include<chrono>
 
 using namespace std;
 class Cell{
+public:  
   bool isClosed;
   bool isFlagged;
   bool isOppened;
@@ -15,7 +16,7 @@ public:
   void setMine();
   void setFlag();
   void setNumOfMines(int n); // ?
-  string getStatus(){
+  string getPrintStatus(){
     string res;
     if (isClosed){
       res="+";
@@ -42,27 +43,31 @@ public:
 class Board
 {
   vector< vector<Cell> > board;
-  
-  clock_t timer;
-  
+    
   int minedSells;
   // Отображаемая информация
   int flaggedCells;
-  int timeSiceStart;
   int size;
-  bool showMined;
+  // Время начала игры (инициализируется в конструкторе)
+  bool isStartTime;
+  chrono::time_point<chrono::steady_clock> st;
+  chrono::time_point<chrono::steady_clock> cur;
+
   // Введенная пользователем команда
   string inpt;
-public:
-  // Вывод доски
-  void printBoard();
+
   void openCell(int x, int y);
-  // Обработать ввод
+  void flagCell(int x, int y);
+  void mineCell(int x, int y);
+
+public:
+  bool isExit;
+  void printBoard();
   void handleUserInput();
   // Конструктор создает доску
   Board(){
-    timer = clock();
-    size = 10; showMined = false;
+    isStartTime = true;
+    size = 10; isExit = false;
     for (int i = 0; i < size; i++){
       vector<Cell> tmpV; 
       for (int j = 0; j < size; j++){
@@ -76,7 +81,12 @@ public:
 };
  
 void Board::printBoard(){
-  string frow = "Число мин : " + to_string(minedSells) + ", Пройденное время : " + to_string(timer) + "\n", zrow = "   ", row; // Создаем пустую строку
+  if (isStartTime){
+    st = chrono::steady_clock::now();
+    isStartTime = false;
+  }
+  cur = chrono::steady_clock::now();
+  string frow = "Число мин : " + to_string(minedSells) + ", Пройденное время : " + to_string(chrono::duration_cast<chrono::seconds> (cur-st).count()) + " с.\n", zrow = "   ", row; // Создаем пустую строку
   string abc = "abcdefghijklmnopqrstuvwxyz";
   int i, j;
   for (i = 0; i < board.size()-1; i++){
@@ -92,7 +102,7 @@ void Board::printBoard(){
   for (i = 0; i < board.size(); i++){
     row = row + to_string(i) + "| ";
     for (j = 0; j < board[i].size(); j++){
-      row = row + (showMined && board[i][j].getStatus() == "*" ? "*" : "+") + " ";
+      row = row + (isExit && board[i][j].getPrintStatus() == "*" ? "*" : "+") + " ";
     }
     row = row + "|" + to_string(i) + "\n";
     cout << row; row.clear();
@@ -102,43 +112,74 @@ void Board::printBoard(){
 
 void Board::handleUserInput(){
   string abc = "abcdefghijklmnopqrstuvwxyz";
-  string prompt = "Введите команду, общий вид : <Ф-ция>_[<Координата x><Координата y>]\nМножество ф-ций : {M - установить мину на указанную ячейку, F - отметить указанную ячейку, O - открыть указанную ячейку, E - Показать расположение всех мин и завершить игру}\nКак указать ячейку ? : Координаты ячейки задаются парой символов : координатой по x - символом из множества : {" + abc.substr(0, size) + "} и координатой по у - цифрой от 0 до 9\n\n>";
+  string prompt = "- Введите команду, общий вид : <Ф-ция>_[<Координата x><Координата y>]\n\nМножество ф-ций : {\n\tM - установить мину на указанную ячейку,\n\tF - отметить указанную ячейку,\n\tO - открыть указанную ячейку,\n\tE - Показать расположение всех мин и завершить игру\n}\n+ Координаты ячейки задаются парой символов : координатой по x - символом из : {a-" + string(1, abc[size-1]) + "} И координатой по у - {0-9}\n\n>";
   cout << prompt;
   cin >> inpt;
   // Переходим к регуляркам, нужно проверить : соответствует ли ввод определенному шаблону 
+  // Доработать эту регулярку
   if (regex_match(inpt, regex("(^[OFM] [a-j][0-9]$)"))){
     if (inpt[0] == 'O') {
       // Вызываем логику игры
       // Надо поставить в соответствие букве цифру
       openCell(abc.find(inpt[2]), inpt[3]);
+    } else if (inpt[0] == 'F'){
+      flagCell(abc.find(inpt[2]), inpt[3]);
+    } else {
+      mineCell(abc.find(inpt[2]), inpt[3]);
     }
   } else if (regex_match(inpt, regex("(^E$)"))){
     // Показываю все мины и завершаю игру
-    showMined = true;
+    isExit = true;
 
   } else {
-    cout << "Команда не распознана" << endl;
+    cout << "\n< Команда не распознана" << endl;
   }
 }
 
 void Board::openCell(int x, int y){
+  // Прописать логику
+}
+
+void Board::mineCell(int x, int y){
+  // Просто сморю минированна ли ячейка ?
+  if (board[x][y].isMined){
+    cout << "[!] Эта ячейка уже содержит мину." << endl;
+  } else {
+    board[x][y].isMined = true;
+  }
+}
+void Board::flagCell(int x, int y){
+  if (board[x][y].isFlagged){
+    board[x][y].isFlagged = false;
+  } else {
+    board[x][y].isFlagged = true;
+  }
+}
+
+void startGame(){
+  cout << "! Игра Сапер !\nВведите команду, множество команд : {\n\t1 -> Начать новую игру,\n\t2 -> Завершить работу программы\n}" << endl;
+  int inpt; bool isExit = false;
+  Board inst;
+  while (!isExit)
+  {
+    cin >> inpt; 
+    if (inpt == 1){
+      inst.printBoard();
+      while (!inst.isExit){
+        inst.handleUserInput();
+        inst.printBoard();
+      }
+    } else if (inpt == 2) {
+      cout << "Работа программы завершена" << endl; isExit = true;
+    } else {
+      cout << "Команда не распознана" << endl;
+    }
+  }
 
 }
 
 int main(){
-  Board brd;
-  bool cont = true;
-  string tmp;
-  while (cont) {
-    brd.printBoard();
-    cout << "Продолжить ? : " << endl;
-    cin >> tmp;
-    if (tmp == "y" || tmp == "yes"){
-      cont = true;
-    } else {
-      cont = false;
-    }
-  }
+  startGame();
   
   return 0;
 }
--- C Programming/Kurgasov_OOP/tst_regex.cpp
+++ C Programming/Kurgasov_OOP/tst_regex.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 #include<string>
 #include<regex>
+#include<chrono>
 using namespace std;
 
 int main()
 {
   string abc = "abcdefghijklmnopqrstuvwxyz", tst = "O e2";
-
+  auto start = chrono::system_clock::now();
   cout << abc.find(tst[2]) << endl; 
   //cout << (regex_match("F a1", regex("(^[OFM] [a-z][0-9]$)")) ? "1" : "0") << endl;
+  auto end = chrono::system_clock::now();
+  cout << chrono::duration<double, milli> (end-start).count() << "ms" << endl;
   return 0;
 }
