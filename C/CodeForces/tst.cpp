6b74c2cf52dee68efd88f32f2182451cfeef8890
Manannikov Anton <anton2manan2@gmail.com>
2023-10-16 19:26:28 +0300

    sync from mac : 16.10.23

---
+++ C/CodeForces/tst.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  // считываем число наборов
+  int t;
+  cin >> t;
+
+  while (t--) {
+    char* str;
+    int strlen; cin >> strlen;
+    str = new char[strlen];
+
+
+  }
+}
