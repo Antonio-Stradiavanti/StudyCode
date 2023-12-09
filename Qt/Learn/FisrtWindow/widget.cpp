34d56cb6705b377afe87eb3d9057d536a9bc8adb
Anton Manannikov <anton2manan2@gmail.com>
2023-12-09 17:40:40 +0300

    sync from  : 09.12.23

--- Qt/Learn/FisrtWindow/widget.cpp
+++
@@ -1,9 +0,0 @@
-#include "widget.h"
-// Конструктор
-Widget::Widget(QWidget *parent)
-    // Вызываем конструктор базового класса
-    : QWidget(parent) {
-    setWindowIcon(QIcon(":/images/qt.png"));
-}
-
-Widget::~Widget() {}
